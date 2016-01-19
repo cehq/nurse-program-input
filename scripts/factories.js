1bb847e9f70f73b878bde75602414b59663d19f1
Jarod Smith <Jarod.Smith@genovatech.com>
2016-01-19 13:24:32 -0600

    initial - localstorage factory

---
+++ scripts/factories.js
@@ -0,0 +1,24 @@
+var appFactories = angular.module('app.factories',[]);
+
+
+appFactories.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      //alert(key);
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    },
+    delete: function(key) {
+      delete $window.localStorage[key];
+    }
+  }
+}]);
+
